3716ae574530f2f086d363baf9b4f1865ca42b81
Satyanarayana Agarwal <satyanarayaa@vmware.com>
2024-06-24 10:46:49 -0400

    first commit

---
+++ src/main/java/com/credhub/credhub_demo/CredhubDemoApplication.java
@@ -0,0 +1,13 @@
+package com.credhub.credhub_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredhubDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CredhubDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/credhub/credhub_demo/CredhubDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.credhub.credhub_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredhubDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
